36546c56185c459d152170c27266e9c83c9daecd
yanjun-ios <1074675572@qq.com>
2021-08-11 13:41:49 +0800

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World 2"}
+		res := &response{Message: "Hello World 3"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
